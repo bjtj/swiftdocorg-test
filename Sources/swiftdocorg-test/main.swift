89511e674fc87c6991eb67730c7336d49e41ac74
TJ <bjtj10@gmail.com>
2021-12-08 10:10:53 +0900

    Update main.swift

--- Sources/swiftdocorg-test/main.swift
+++ Sources/swiftdocorg-test/main.swift
@@ -5,12 +5,14 @@ print("Hello, world!")
  A class
  */
 public class A {
+ 
+    public var name: String = "name"  //: name value
 
     public func a() {
     }
 
     /**
-     this is function `b`
+     This is function `b`
      - Parameter param1: string param1
      */
     public func b(param1: String) {
